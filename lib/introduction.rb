1242c6698816be784d9d22fdc688879e7a5f7d87
brad hemmerick <brad.hemmerick13@gmail.com>
2019-10-21 18:22:54 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,11 +8,11 @@
  def introduction(name)
   puts "Hi, my name is #{name}."
 end
-def introduction_with_language(name,language)
-  "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-def introduction_with_language_optional(name,language="ruby")
-  "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language(name, language="ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
